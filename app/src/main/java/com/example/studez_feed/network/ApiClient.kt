f9e604ab7170155ca1f981c62be7d946b4dbd67f
simon <simongunyali@gmail.com>
2025-03-17 17:08:54 +0300

    "fixed the registration errors"

--- app/src/main/java/com/example/studez_feed/network/ApiClient.kt
+++ app/src/main/java/com/example/studez_feed/network/ApiClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "https://prime-squirrel-inherently.ngrok-free.app/api/" // ✅ 10.0.2.2 works for localhost in Android Emulator
+    private const val BASE_URL = "https://prime-squirrel-inherently.ngrok-free.app/api/" // 10.0.2.2 works for localhost in Android Emulator
 
     val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
